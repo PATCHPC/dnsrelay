74d5e1a724ee021afd449302fca9fd51c9f18bd2
buptpanchang@gmail.com <42539366+PATCHPC@users.noreply.github.com>
2020-07-26 14:33:31 +0800

    添加项目文件。

---
+++ header.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+#pragma comment(lib,"wsock32.lib")
+
+
+
+
+
+typedef struct {
+	char* IP;
+	char* domain;
+}IPTranslate;
+
+typedef struct {
+	unsigned short formerID;
+	bool DONE;
+}IPTransform;
+
+
+//读取DNS请求中的域名
+
+
+//判断是否在表中找到DNS请求中的域名，找到返回下标
+
+
+//将请求ID转换为新的ID并写入ID转换表中
+
+
+//函数：读取域名解析表
+int ReadTable(char* tablePath);
+
+
+//IO：打印时间、新id、功能、域名、IP
+void DisplayInfo(unsigned short newID, int find);
---
+++ io.cpp
@@ -0,0 +1,13 @@
+﻿#include "header.h"
+
+//IO：读取域名解析表并返回域名解析表中的条目个数
+int ReadTable(char* tablePath) {
+	return
+}
+
+
+
+//IO：打印时间、新id、功能、域名、IP
+void DisplayInfo(unsigned short newID, int find) {
+
+}
